fdf4e99300623744b412cd7b343ca099228e8f2d
Yuchen Wang <ycwang0712@gmail.com>
2018-02-25 23:51:37 -0500

    Implemented dplyr compatibility schema
    http://dplyr.tidyverse.org/articles/compatibility.html#dplyr-0-6-0

--- R/check_missing.R
+++ R/check_missing.R
@@ -12,21 +12,45 @@
 #' @author Min Ma
 #' @export
 check_missing <- function(data, ..., ret_prop = TRUE) {
-  check_missing_(data, .dots = lazyeval::lazy_dots(...),
-                 ret_prop = ret_prop)
+  UseMethod("check_missing")
 }
 
-#' @describeIn check_missing SE version of check_missing.
+#' @describeIn check_missing S3 method for data.frame.
 #' @export
-check_missing_ <- function(data, ..., .dots, ret_prop = TRUE) {
-  dots <- lazyeval::all_dots(.dots, ...)
-  vars <- dplyr::select_vars_(names(data), dots)
+check_missing.data.frame <- function(data, ..., ret_prop = TRUE) {
+  # main method for NSE function
+  dots <- quos(...)
 
-  if (ret_prop) {
-    fun <- dplyr::funs_(quote(sum(is.na(.)) / n()))
-  } else {
-    fun <- dplyr::funs_(quote(sum(is.na(.))))
+  if (length(dots) == 0) {
+    stop("Please specify unquoted column names, tidy select helpers supported.")
   }
 
-  dplyr::summarise_at(data, .cols = vars, .funs = fun)
+  if (ret_prop) {
+    fun <- dplyr::funs(sum(is.na(.)) / n())
+  } else {
+    fun <- dplyr::funs(sum(is.na(.)))
+  }
+
+  dplyr::summarise_at(data, .vars = dplyr::vars(UQS(dots)), .funs = fun)
+}
+
+#' @describeIn check_missing default method for new NSE generic that dispatches
+#' to the NSE generic for backward compatibility.
+#' @export
+check_missing.default <- function(data, ...) {
+  check_missing_(.data, .dots = compat_as_lazy_dots(...))
+}
+
+#' @export
+check_missing_ <- function(ata, ..., .dots = list(),
+                           ret_prop = TRUE) {
+  # to provide backward compatibility to the old SE function
+  UseMethod("check_missing_")
+}
+
+#' @export
+check_missing_.data.frame <- function(data, ..., .dots = list(),
+                                      ret_prop = TRUE) {
+  dots <- compat_lazy_dots(.dots, rlang::caller_env(), ...)
+  check_missing(data, UQS(dots), ret_prop = ret_prop)
 }
